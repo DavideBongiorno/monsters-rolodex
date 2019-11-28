dd672273b088144da4d906ec00d10a5561153c76
DavideBongiorno <davide@create-react-app.it>
2019-11-28 14:32:19 +0100

    Updates

---
+++ precache-manifest.54a250f5c26d18fb88e3f5b9d0533749.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e692588b096b6100d418ac65aaf11bc3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bc1b170b9720f7cbeb6c",
+    "url": "/monsters-rolodex/static/css/main.abf4f8f0.chunk.css"
+  },
+  {
+    "revision": "d930919d12739d830bf5",
+    "url": "/monsters-rolodex/static/js/2.f7ef8a42.chunk.js"
+  },
+  {
+    "revision": "bc1b170b9720f7cbeb6c",
+    "url": "/monsters-rolodex/static/js/main.b6278320.chunk.js"
+  },
+  {
+    "revision": "900deb9510f64772388c",
+    "url": "/monsters-rolodex/static/js/runtime-main.2092e7f4.js"
+  }
+]);
